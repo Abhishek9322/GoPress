aa1fc88ed4252e1acf7469355a51b771c64c5126
Abhishek <abhi9322395458@gmail.com>
2026-06-08 21:17:46 +0530

    Add the Accept Order Reject Order from Shopowner side

--- GoPress.Api/Controllers/ShopController.cs
+++ GoPress.Api/Controllers/ShopController.cs
@@ -1,11 +1,62 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using GoPress.Api.Extensions;
+using GoPress.Application.Features.Orders.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GoPress.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "ShopOwner")]
     public class ShopController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        public ShopController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Authorize(Roles = "ShopOwner")]
+        [HttpPut("accept-order/{orderId}")]
+        public async Task<IActionResult>AcceptOrder(int orderId)
+        {
+            var currentUser = User.GetCurrentUser();
+            if (currentUser == null)
+            {
+                return Unauthorized();
+            }
+            var command = new AcceptOrderCommand
+            {
+                OrderId = orderId,
+               ShopOwnerId=currentUser.UserId
+            };
+
+            var response = await _mediator.Send(command);
+            return Ok(response);
+
+        }
+
+
+        [Authorize(Roles = "ShopOwner")]
+        [HttpPut("reject-order/{orderId}")]
+        public async Task<IActionResult> RejectOrder(int orderId)
+        {
+            var currentUser = User.GetCurrentUser();
+
+            var command = new RejectOrderCommand
+            {
+                OrderId = orderId,
+                ShopOwnerId = currentUser.UserId
+            };
+
+            var response =
+                await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+
     }
 }
---
+++ GoPress.Application/Features/Orders/Commands/AcceptOrderCommand.cs
@@ -0,0 +1,16 @@
+﻿using GoPress.Application.Features.Orders.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoPress.Application.Features.Orders.Commands
+{
+    public class AcceptOrderCommand:IRequest<Response<string>>
+    {
+        public int OrderId { get; set; }
+        public int ShopOwnerId { get; set; }
+    }
+}
