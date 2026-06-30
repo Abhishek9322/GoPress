cbc6164dd00f93bc4c2bdee0ed2223de63a7bae7
Abhishek <abhi9322395458@gmail.com>
2026-06-30 20:45:37 +0530

    Add the getallorderforthe admin acording to the status

--- GoPress.Application/Comman/Caching/CacheKeys.cs
+++ GoPress.Application/Comman/Caching/CacheKeys.cs
@@ -9,6 +9,16 @@ namespace GoPress.Application.Comman.Caching
     public static class CacheKeys
     {
         public const string AdminDashboard = "AdminDashboard";  //add unther with time like order ,user etc 
-     
+
+
+        public const string PendingOrders = "PendingOrders";
+
+        public const string CompletedOrders = "CompletedOrders";
+
+        public const string RejectedOrders = "RejectedOrders";
+
+        public const string AcceptedOrders = "AcceptedOrders";
+
+        public const string CancelledOrders = "CancelledOrders";
     }
 }
