2351efadc878b10ef9b6d2c1cf9a00cd2e17c9ab
Abhishek <abhi9322395458@gmail.com>
2026-06-30 21:15:04 +0530

    Add the get All pending order by Admin to see the order pending with proper caching

---
+++ GoPress.Application/DTOs/Admin/AdminOrderDto.cs
@@ -0,0 +1,31 @@
+﻿using GoPress.Application.DTOs.Orders;
+using GoPress.Domain.Enums;
+
+namespace GoPress.Application.DTOs.Admin
+{
+    public class AdminOrderDto
+    {
+        public int OrderId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string ShopOwnerName { get; set; }
+
+        public string? DeliveryBoyName { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public OrderStatusEnum Status { get; set; }
+
+        public DateTime PickupDate { get; set; }
+
+        public DateTime? DeliveryDate { get; set; }
+
+        public string PickupAddress { get; set; }
+
+        public string DeliveryAddress { get; set; }
+
+        public List<OrderItemResponseDto> OrderItems { get; set; }
+            = new();
+    }
+}
