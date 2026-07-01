e4234e98e07687c7cbd7cc1c59f5eb9812472dcf
Abhishek <abhi9322395458@gmail.com>
2026-07-01 20:23:12 +0530

    Add the functiona for the geting the All Rejected order at admin side

---
+++ GoPress.Application/Features/AdminOrdersmanagment/GetAllOrderByStatus/Queries/GetAllRejectedOrderByAdminQuery.cs
@@ -0,0 +1,15 @@
+﻿using GoPress.Application.DTOs.Admin;
+using GoPress.Application.Features.Orders.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoPress.Application.Features.AdminOrdersmanagment.GetAllOrderByStatus.Queries
+{
+    public class GetAllRejectedOrderByAdminQuery:IRequest<Response<List<AdminOrderDto>>>
+    {
+    }
+}
