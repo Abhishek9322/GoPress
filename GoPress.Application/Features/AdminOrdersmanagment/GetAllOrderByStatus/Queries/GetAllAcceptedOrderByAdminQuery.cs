36ea052bec90073bf528930bc9c8103a155973c8
Abhishek <abhi9322395458@gmail.com>
2026-07-01 20:36:48 +0530

    Add function for the get the all Accepted order for the Admin side

---
+++ GoPress.Application/Features/AdminOrdersmanagment/GetAllOrderByStatus/Queries/GetAllAcceptedOrderByAdminQuery.cs
@@ -0,0 +1,15 @@
+﻿using GoPress.Application.DTOs.Admin;
+using GoPress.Application.Features.Orders.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoPress.Application.Features.AdminOrdersmanagment.GetAllOrderByStatus.Queries
+{
+    public class GetAllAcceptedOrderByAdminQuery:IRequest<Response<List<AdminOrderDto>>>
+    {
+    }
+}
