3560be56c19e3c5cd953d187fbbfbb2cf51dea69
Abhishek <abhi9322395458@gmail.com>
2026-07-15 21:57:39 +0530

    Add the customer all order

---
+++ GoPress.Mvc/Areas/Customer/Models/OrderItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace GoPress.Mvc.Areas.Customer.Models
+{
+    public class OrderItemViewModel
+    {
+        public int Id { get; set; }
+
+        public string ClothName { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
