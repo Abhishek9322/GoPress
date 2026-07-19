a00b1125bf0e71f1ca0cc765cf7ef39266de5e8b
Abhishek <abhi9322395458@gmail.com>
2026-07-19 20:48:36 +0530

    Add the api for the search shop by name or the owner name not city or the open or slose

---
+++ GoPress.Application/Interfaces/Repositories/ISearchRepository.cs
@@ -0,0 +1,14 @@
+﻿using GoPress.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoPress.Application.Interfaces.Repositories
+{
+    public interface ISearchRepository
+    {
+        Task<List<ApplicationUser>> SearchShopsAsync(string keyword);
+    }
+}
---
+++ GoPress.Infrastructure/Repositories/SearchRepository.cs
@@ -0,0 +1,40 @@
+﻿using GoPress.Application.Interfaces.Repositories;
+using GoPress.Domain.Entities;
+using GoPress.Domain.Enums;
+using GoPress.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoPress.Infrastructure.Repositories
+{
+    public class SearchRepository : ISearchRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public SearchRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<ApplicationUser>> SearchShopsAsync(string keyword)
+        {
+            keyword = keyword.Trim().ToLower();
+            return await _context.ApplicationUsers
+                .Include(x => x.ShopOwnerProfile)
+                .Where(x =>
+                    x.Role == UserRoleenum.ShopOwner &&
+                    x.ShopOwnerProfile != null &&
+                    (
+                        x.ShopOwnerProfile.ShopName.ToLower().Contains(keyword)
+                        ||
+                        x.FullName.ToLower().Contains(keyword)
+                    )
+                )
+                .OrderBy(x => x.ShopOwnerProfile.ShopName)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
