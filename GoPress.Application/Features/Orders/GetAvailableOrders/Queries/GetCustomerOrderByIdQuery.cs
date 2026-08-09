164e00a01518e6caeb56cd5a867df7dcdb627005
Abhishek <abhi9322395458@gmail.com>
2026-08-09 20:50:35 +0530

    Add the one page for the view details of the order for the customer here

---
+++ GoPress.Application/Features/Orders/GetAvailableOrders/Queries/GetCustomerOrderByIdQuery.cs
@@ -0,0 +1,17 @@
+﻿using GoPress.Application.DTOs.Orders;
+using GoPress.Application.Features.Orders.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoPress.Application.Features.Orders.GetAvailableOrders.Queries
+{
+    public class GetCustomerOrderByIdQuery:IRequest<Response<OrderResponseDto>>
+    {
+        public int CustomerId { get; set; }
+        public int OrderId { get; set; }
+    }
+}
