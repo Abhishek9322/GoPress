e9dcae5f5d18a6c78114542c70a398d4a79907a2
Abhishek <abhi9322395458@gmail.com>
2026-08-25 20:22:36 +0530

    Create the All Rejected page for the shopOwner

---
+++ GoPress.Mvc/Areas/ShopOwner/Models/AllRejectedOrderShopOwner.cs
@@ -0,0 +1,20 @@
+﻿namespace GoPress.Mvc.Areas.ShopOwner.Models
+{
+    public class AllRejectedOrderShopOwner
+    {
+        public int OrderId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerPhone { get; set; }
+
+        public string PickupAddress { get; set; }
+
+        public string DeliveryAddress { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public string Status { get; set; }
+    }
+
+}
