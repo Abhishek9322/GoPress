8a151405dede4a3b9d6dc8ff6c5740332070a9ee
Abhishek <abhi9322395458@gmail.com>
2026-09-15 20:51:26 +0530

    Add the Api for the Out for Delivery by shopOwner

---
+++ GoPress.Application/Features/Orders/ProcessingOrder/CommandHandler/OutForDeliveryOrdersQueryHandler.cs
@@ -0,0 +1,72 @@
+﻿using GoPress.Application.Comman.Caching;
+using GoPress.Application.Features.Orders.ProcessingOrder.Command;
+using GoPress.Application.Features.Orders.Responses;
+using GoPress.Application.Interfaces.Caching;
+using GoPress.Application.Interfaces.Repositories;
+using GoPress.Domain.Enums;
+using MediatR;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoPress.Application.Features.Orders.ProcessingOrder.CommandHandler
+{
+    public class OutForDeliveryOrdersQueryHandler : IRequestHandler<OutForDeliveryOrdersQuery, Response<string>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ILogger<OutForDeliveryOrdersQueryHandler> _logger;
+        private readonly ICacheService _cacheService;
+        public OutForDeliveryOrdersQueryHandler(IOrderRepository orderRepository,ILogger<OutForDeliveryOrdersQueryHandler> logger,ICacheService cacheService)
+        {
+            _orderRepository = orderRepository;
+            _logger = logger;
+            _cacheService = cacheService;
+        }
+        public async Task<Response<string>> Handle(OutForDeliveryOrdersQuery request, CancellationToken cancellationToken)
+        {
+            var order = await _orderRepository.GetByIdAsync(request.OrderId);
+
+            if (order == null)
+            {
+                return new Response<string>(
+                    "Order Not Found");
+            }
+
+            if(order.ShopOwnerId != request.ShopOwnerId)
+            {
+                return new Response<string>(
+                    "You cannot manage this order");
+            }
+
+            if (order.Status != OrderStatusEnum.ReadyForDelivery)
+            {
+                return new Response<string>(
+                    "Order is not in ReadyForDelivery state");
+            }
+
+            order.Status = OrderStatusEnum.OutForDelivery;
+
+            _logger.LogInformation(
+                    "Order {OrderId} marked OutForDelivery by ShopOwner {ShopOwnerId}",
+                 request.OrderId,
+                request.ShopOwnerId);
+
+            await _orderRepository.UpdateAsync(order);
+
+            await _cacheService.RemoveAsync(CacheKeys.AdminDashboard);
+            await _cacheService.RemoveAsync(CacheKeys.ShopOwnerDashboard);
+            await _cacheService.RemoveAsync(CacheKeys.DeliveryBoyDashboard);
+            await _cacheService.RemoveAsync(CacheKeys.CustomerDashboard);
+
+            _logger.LogInformation(
+                    "Cache cleared for AdminDashboard, ShopOwnerDashboard, DeliveryBoyDashboard, and CustomerDashboard after Order {OrderId} marked OutForDelivery",
+                 request.OrderId);
+
+            return new Response<string>("Order Out For Delivery .");
+        }
+    }
+}
